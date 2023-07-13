4eb243b43a7690b259f2dbc52169b6df0e8bbd4b
foreknowledge <kyj_0129@naver.com>
2023-07-13 23:24:03 +0900

    Add test buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import './App.css';
+
 function App() {
-  return <h1>Home</h1>;
+  return (
+    <main className="container">
+      <h1>Home</h1>
+      <p>Click the buttons below to see the dialogs</p>
+      <div className="buttons">
+        <button>alert</button>
+        <button>confirm</button>
+        <button>prompt</button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
